5bed850cf8e24d5285e232ba194d7d152d94fe33
0407-zh <derekchan0407@gmail.com>
2022-12-17 22:23:13 +0800

    add mini compiler

---
+++ compiler/compiler.go
@@ -0,0 +1,77 @@
+package compiler
+
+import (
+	"Monkey/ast"
+	"Monkey/code"
+	"Monkey/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions // 保存生成的字节码
+	constants    []object.Object   // 常量池
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			if err := c.Compile(s); err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		if err := c.Compile(node.Expression); err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		if err := c.Compile(node.Left); err != nil {
+			return err
+		}
+		if err := c.Compile(node.Right); err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+
+	return nil
+}
+
+// 将object添加到常量池
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+// 生成指令并将其添加到最终结果
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...) // 构建指令
+	pos := c.addInstruction(ins)
+	return pos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
